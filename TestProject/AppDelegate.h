a038e1684b9793ddbd50eaf34abec4457e7ac2b6
Begum D <begumd@begums-macbook-pro.local>
2015-12-08 10:42:19 +0100

    Initial Commit

---
+++ TestProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestProject
+//
+//  Created by Begum D on 08/12/15.
+//  Copyright © 2015 Begum D. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestProject
+//
+//  Created by Begum D on 08/12/15.
+//  Copyright © 2015 Begum D. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
